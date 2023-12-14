f52b421822eed397b3191bc121d6f818be4b4a84
JM0907 <135572091+JM0907@users.noreply.github.com>
2023-12-14 18:46:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,280 +1,100 @@
-from flask import Flask,render_template
-import requests
-from dotenv import load_dotenv,dotenv_values
-app=Flask(__name__)
+from flask import Flask, render_template, jsonify, request
+import requests 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
 
-config = dotenv_values('.env')
+app = Flask(__name__)
 
-app = Flask (__name__)
-def get_weather_data (city):
-    API_KEY = config['API_KEY']
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang&appid={API_KEY}'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokedex.sqlite"
+
+db = SQLAlchemy(app)
+
+class Pokemon(db.Model):
+    id:Mapped[int] = mapped_column(db.Integer, primary_key=True,autoincrement=True)
+    name:Mapped[str]= mapped_column(db.String,nullable=False)
+    height:Mapped[float] = mapped_column(db.Float,nullable=False)
+    weight: Mapped[float] = mapped_column(db.Float, nullable=False)
+    order: Mapped[int] = mapped_column(db.Integer,nullable=False)
+    type :Mapped[str]= mapped_column(db.String,nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+def get_pokemon_data(pokemon):
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
     r = requests.get(url).json()
-    print(r)
     return r
 
-@app.route('/prueba')
-def prueba():
-    clima=get_weather_data('london')
-    temperatura=str(clima['main']['temp'])
-    descripcion= str(clima['weather'][0]['description'])
-    icono=str(clima['weather'][0]['icon'])
-    r_json={
-        'ciudad': 'london',
-        'temperatura':temperatura,
-        'descripcion': descripcion,
-        'icono': icono
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name_pokemon = request.form.get('name')
+        if name_pokemon:
+            data = get_pokemon_data(name_pokemon)
+        Pokemon = {
+        'photo': data.get['sprites']['other']['official-artwork']['front_default'],
+        'name': data.get['name'].upper(),
+        'height': data.get['height'],
+        'weight': data.get['weight'],
+        'order': data.get['order'], 
+        'type': ''
         }
-    return render_template('weather.html', clima = r_json)
-   
-
-
-
-@app.route('/about')
-def hello_CV():
-    return render_template('CV.html')
-
-
-if __name__ == '__main__':
-    app.run(debug = True)
-
-@app.route('/clima')
-def clima_page():
-    return render_template('resultado.json')
-
-@app.route('/clima')
-def clima():
-    return 'CLIMA'
-
-if __name__ == '__main__':
-    app.run(debug = True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return search_pokemon(name_pokemon)
+
+    return render_template('pokemon.html', pokemon=None)
+
+@app.route("/pokemon/<name>")
+def search_pokemon(name):
+    data = get_pokemon_data(name.lower())
+    if 'sprites' not in data or 'official-artwork' not in data['sprites']['other']:
+        return render_template('pokemon.html', pokemon=None)
+    
+    pokemon = {
+        'photo': data['sprites']['other']['official-artwork']['front_default'],
+        'name': data['name'].upper(),
+        'height': data['height'],
+        'weight': data['weight'],
+        'order': data['order'], 
+        'type': '',
+    }
+    return render_template('pokemon.html', pokemon=pokemon)
+
+@app.route("/detalle")
+def detalle():
+    return render_template('detalle.html')
+
+@app.route("/insert")
+def insert():
+    new_pokemon= 'Ditto'
+    if new_pokemon:
+            obj = Pokemon(name=new_pokemon,height=1.75, weight=100, order=100, type='Normal')
+            db.session.add(obj)
+            db.session.commit()
+    return 'Pokemon Agregado'
+
+@app.route("/select")
+def select():
+    lista_pokemon= Pokemon.query.all()
+    for p in lista_pokemon:
+        print(p.name)
+    return 'alo'
+
+@app.route("/selectbyname/<name>")
+def selectbyname(name):
+    poke= Pokemon.query.filter_by(name=name).first()
+    return str(poke.id),str(poke.name)
+
+@app.route("/selectbyid/<id>")
+def selectbyid(id):
+    poke = Pokemon.query.filter_by(id=id).first()
+    return str(poke.id) + str(poke.name)
+
+@app.route("/deletetbyid/<id>")
+def deletetbyid(id):
+    pokemon_a_eliminar = Pokemon.query.filter_by(id=id).first()
+    db.session.delete(pokemon_a_eliminar)
+    db.session.commit()
+    return 'Pokemon Eliminado'
+
+if __name__=='__main__':
+    app.run(debug=True)
